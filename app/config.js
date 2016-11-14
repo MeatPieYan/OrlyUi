87e4fa7beb0fdb0283fce1d6de131cb0c0d176ef
MeatPieYan <261893666@qq.com>
2016-11-14 16:45:21 +0800

    update config.js

--- app/config.js
+++ app/config.js
@@ -1,3 +1,7 @@
+console.log('node_env=================>',process.env.NODE_ENV);
+
+const serverUrl = process.env.NODE_ENV==='production'?'45.32.42.0:10630':'127.0.0.1:10630'
+
 export default {
-  serverUrl: process.env.NODE_ENV==='production'?'45.32.42.0:10630':'127.0.0.1:10630'
+  serverUrl
 }
