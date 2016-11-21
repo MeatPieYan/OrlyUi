332f10bff33b29db927c12f9c17fa277c3139a90
MeatPieYan <261893666@qq.com>
2016-11-21 11:55:16 +0800

    consolidate test file into test folder

--- test/component/header/Header.test.js
+++ test/component/header/Header.test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Header from './Header';
+import Header from '../../../app/component/header/Header';
 
 test('shallow', t => {
   const wrapper = shallow(<Header />);
